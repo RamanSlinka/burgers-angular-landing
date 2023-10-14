4b476b044961ae541623d487474a41ed56caf4e6
Roman <romuch1986@gmail.com>
2023-10-14 14:10:46 +0200

    go to local Data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,103 +29,152 @@ export class AppComponent {
 
   public productsData: any =[]
 
-  // productsData = [
-  //   {
-  //     image: "1.png",
-  //     title: 'Burger cheddar & bacon',
-  //     text: 'Crispy beef cutlet, bun, tomato, Cheddar cheese, brisket, red onion, iceberg lettuce, mayonnaise, ketchup, cheese sauce',
-  //     price: 8,
-  //     basePrice: 8,
-  //     grams: 360
-  //   },
-  //   {
-  //     image: "2.png",
-  //     title: 'BBQ with chicken',
-  //     text: 'Sesame brioche bun, chicken cutlet, cheddar cheese, tomato, pickled cucumber, pickled onion, BBQ sauce.',
-  //     price: 9,
-  //     basePrice: 9,
-  //     grams: 390
-  //   },
-  //   {
-  //     image: "3.png",
-  //     title: 'Double beef burger',
-  //     text: 'Two beef patties, cheddar cheese, romaine lettuce, pickled cucumbers, fresh tomato, bacon, red onion, burger sauce, mustard.',
-  //     price: 10,
-  //     basePrice: 10,
-  //     grams: 420
-  //   },
-  //   {
-  //     image: "4.png",
-  //     title: 'Bavarian burger',
-  //     text: 'Burger bun, beef patty, red onion, cheese, hunting sausage, barbecue sauce, cheese sauce, iceberg lettuce.',
-  //     price: 7,
-  //     basePrice: 7,
-  //     grams: 320
-  //   },
-  //   {
-  //     image: "5.png",
-  //     title: 'Bacon cheeseburger',
-  //     text: 'Burger bun, beef cutlet, brisket, tomato, pickled cucumber, cheese, cheese sauce, ketchup, herbs.',
-  //     price: 8,
-  //     basePrice: 8,
-  //     grams: 360
-  //   },
-  //   {
-  //     image: "6.png",
-  //     title: 'Indiana burger',
-  //     text: 'Burger bun, chicken cutlet, brisket, egg, pickled cucumber, crispy onion, ketchup, cheese sauce, mustard, greens.',
-  //     price: 10,
-  //     basePrice: 10,
-  //     grams: 410
-  //   },
-  //   {
-  //     image: "7.png",
-  //     title: 'Veggie burger',
-  //     text: 'Burger bun, veggie patty, red onion, cheese, fresh tomato, barbecue sauce, cheese sauce, iceberg lettuce.',
-  //     price: 8,
-  //     basePrice: 8,
-  //     grams: 290
-  //   },
-  //   {
-  //     image: "8.png",
-  //     title: 'Weepy Joe',
-  //     text: 'Burger bun, beef patty, brisket, tomato, pickled cucumber, red onion, cheese, jalapeno pepper, ketchup, herbs.',
-  //     price: 7,
-  //     basePrice: 7,
-  //     grams: 360
-  //   },
-  //   {
-  //     image: "9.png",
-  //     title: 'Double cheeseburger',
-  //     text: 'Burger bun, two beef patties, double cheddar cheese, crispy onion, ketchup, cheese sauce, mustard, herbs.',
-  //     price: 11,
-  //     basePrice: 11,
-  //     grams: 420
-  //   },
-  //   {
-  //     image: "10.png",
-  //     title: 'Fresh-burger',
-  //     text: 'Burger bun, beef patty, bacon, cheddar cheese, egg, salami, barbecue sauce, cheese sauce, iceberg lettuce, fresh tomato.',
-  //     price: 9,
-  //     basePrice: 9,
-  //     grams: 310
-  //   },
-  //   {
-  //     image: "11.png",
-  //     title: 'Zucchini burger',
-  //     text: 'Burger bun, veggie chickpea patty, grilled zucchini, tomato, pickled cucumber, cheese, mustard sauce, ketchup, herbs.',
-  //     price: 8,
-  //     basePrice: 8,
-  //     grams: 330
-  //   }, {
-  //     image: "12.png",
-  //     title: 'Double Cheddar Burger',
-  //     text: 'Burger bun, beef patty, brisket, red onion, pickled cucumber, tomato, ketchup, double cheddar cheese, mustard, herbs.\n',
-  //     price: 9,
-  //     basePrice: 9,
-  //     grams: 370
-  //   },
-  // ]
+  public productsDataLocal: item [] = [
+    {
+      image: "1.png",
+      title: 'Burger cheddar & bacon',
+      text: 'Crispy beef cutlet, bun, tomato, Cheddar cheese, brisket, red onion, iceberg lettuce, mayonnaise, ketchup, cheese sauce',
+      price: 8,
+      basePrice: 8,
+      grams: 360
+    },
+    {
+      image: "2.png",
+      title: 'BBQ with chicken',
+      text: 'Sesame brioche bun, chicken cutlet, cheddar cheese, tomato, pickled cucumber, pickled onion, BBQ sauce.',
+      price: 9,
+      basePrice: 9,
+      grams: 390
+    },
+    {
+      image: "3.png",
+      title: 'Double beef burger',
+      text: 'Two beef patties, cheddar cheese, romaine lettuce, pickled cucumbers, fresh tomato, bacon, red onion, burger sauce, mustard.',
+      price: 10,
+      basePrice: 10,
+      grams: 420
+    },
+    {
+      image: "4.png",
+      title: 'Bavarian burger',
+      text: 'Burger bun, beef patty, red onion, cheese, hunting sausage, barbecue sauce, cheese sauce, iceberg lettuce.',
+      price: 7,
+      basePrice: 7,
+      grams: 320
+    },
+    {
+      image: "5.png",
+      title: 'Bacon cheeseburger',
+      text: 'Burger bun, beef cutlet, brisket, tomato, pickled cucumber, cheese, cheese sauce, ketchup, herbs.',
+      price: 8,
+      basePrice: 8,
+      grams: 360
+    },
+    {
+      image: "6.png",
+      title: 'Indiana burger',
+      text: 'Burger bun, chicken cutlet, brisket, egg, pickled cucumber, crispy onion, ketchup, cheese sauce, mustard, greens.',
+      price: 10,
+      basePrice: 10,
+      grams: 410
+    },
+    {
+      image: "7.png",
+      title: 'Veggie burger',
+      text: 'Burger bun, veggie patty, red onion, cheese, fresh tomato, barbecue sauce, cheese sauce, iceberg lettuce.',
+      price: 8,
+      basePrice: 8,
+      grams: 290
+    },
+    {
+      image: "8.png",
+      title: 'Weepy Joe',
+      text: 'Burger bun, beef patty, brisket, tomato, pickled cucumber, red onion, cheese, jalapeno pepper, ketchup, herbs.',
+      price: 7,
+      basePrice: 7,
+      grams: 360
+    },
+    {
+      image: "9.png",
+      title: 'Double cheeseburger',
+      text: 'Burger bun, two beef patties, double cheddar cheese, crispy onion, ketchup, cheese sauce, mustard, herbs.',
+      price: 11,
+      basePrice: 11,
+      grams: 420
+    },
+    {
+      image: "10.png",
+      title: 'Fresh-burger',
+      text: 'Burger bun, beef patty, bacon, cheddar cheese, egg, salami, barbecue sauce, cheese sauce, iceberg lettuce, fresh tomato.',
+      price: 9,
+      basePrice: 9,
+      grams: 310
+    },
+    {
+      image: "11.png",
+      title: 'Zucchini burger',
+      text: 'Burger bun, veggie chickpea patty, grilled zucchini, tomato, pickled cucumber, cheese, mustard sauce, ketchup, herbs.',
+      price: 8,
+      basePrice: 8,
+      grams: 330
+    },
+    {
+      image: "12.png",
+      title: 'Double Cheddar Burger',
+      text: 'Burger bun, beef patty, brisket, red onion, pickled cucumber, tomato, ketchup, double cheddar cheese, mustard, herbs.',
+      price: 9,
+      basePrice: 9,
+      grams: 370
+    },
+    {
+      image: "13.png",
+      title: 'Black White Light',
+      text: 'Black and white burger bun, beef patty, Parmesan cheese, hot sauce.',
+      price: 12,
+      basePrice: 12,
+      grams: 290
+    },
+    {
+      image: "14.png",
+      title: 'Black White Bacon',
+      text: 'Black and white burger bun, beef patty, bacon, Cheddar cheese, iceberg lettuce, sauce, cucumber.',
+      price: 13,
+      basePrice: 13,
+      grams: 310
+    },
+    {
+      image: "15.png",
+      title: 'Black White Max',
+      text: 'Black and white burger bun, beef patty, Cheddar cheese, iceberg lettuce, egg, black sauce.',
+      price: 16,
+      basePrice: 16,
+      grams: 330
+    },
+    {
+      image: "16.png",
+      title: 'Black Star Light',
+      text: 'Black burger bun, chicken cutlet, egg, iceberg lettuce, fresh tomato, cucumber, Black sauce.',
+      price: 13,
+      basePrice: 13,
+      grams: 290
+    },
+    {
+      image: "17.png",
+      title: 'Black Star',
+      text: 'Black burger bun, beef patty, Cheddar cheese, iceberg lettuce, fresh tomato, cucumber.',
+      price: 14,
+      basePrice: 14,
+      grams: 310
+    },
+    {
+      image: "18.png",
+      title: 'Black Star Max',
+      text: 'Black burger bun, beef cutlet, Bacon, Onion, Cheddar cheese, iceberg lettuce, fresh tomato, ketchup.',
+      price: 15,
+      basePrice: 15,
+      grams: 330
+    },
+  ]
 
   constructor(private fb: FormBuilder, private appService: AppService) {
   }
@@ -178,7 +227,7 @@ export class AppComponent {
     }
     this.currency = newCurrency;
 
-    this.productsData.forEach((item: item) => {
+    this.productsDataLocal.forEach((item: item) => {
       item.price = +(item.basePrice * coefficient).toFixed(1)
     })
   }
