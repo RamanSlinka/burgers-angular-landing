b48680ffce947b9246cbb8227e6ebc4efb700059
Roman <romuch1986@gmail.com>
2023-10-15 22:24:25 +0200

    added internationalization & localization to project . Used Angular i18n (https://lokalise.com/blog/angular-i18n/)
    ng extract-i18n  (for add translate files)
    ng serve --configuration=ru --open (for starting local server : 'ru' or 'en')

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,150 +26,159 @@ export class AppComponent {
   })
 
   public currency = '$';
+  public language: string = 'English';
+
+  public localesList = [
+      {code:'en-US', label: 'English'},
+      {code:'ru', label: 'Русский'},
+  ]
 
   public productsData: any =[]
 
   public productsDataLocal: item [] = [
     {
       image: "1.png",
-      title: 'Burger cheddar & bacon',
-      text: 'Crispy beef cutlet, bun, tomato, Cheddar cheese, brisket, red onion, iceberg lettuce, mayonnaise, ketchup, cheese sauce',
+      title: $localize`Burger cheddar & bacon`,
+      text:  $localize`Crispy beef cutlet, bun, tomato, Cheddar cheese, brisket,
+      red onion, iceberg lettuce, mayonnaise, ketchup, cheese sauce`,
       price: 8,
       basePrice: 8,
       grams: 360
     },
     {
       image: "2.png",
-      title: 'BBQ with chicken',
-      text: 'Sesame brioche bun, chicken cutlet, cheddar cheese, tomato, pickled cucumber, pickled onion, BBQ sauce.',
+      title:  $localize`BBQ with chicken`,
+      text:  $localize`'Sesame brioche bun, chicken cutlet, cheddar cheese, tomato, pickled cucumber,
+      pickled onion, BBQ sauce.`,
       price: 9,
       basePrice: 9,
       grams: 390
     },
     {
       image: "3.png",
-      title: 'Double beef burger',
-      text: 'Two beef patties, cheddar cheese, romaine lettuce, pickled cucumbers, fresh tomato, bacon, red onion, burger sauce, mustard.',
+      title:  $localize`Double beef burger`,
+      text:  $localize`Two beef patties, cheddar cheese, romaine lettuce, pickled cucumbers, fresh tomato,
+      bacon, red onion, burger sauce, mustard.`,
       price: 10,
       basePrice: 10,
       grams: 420
     },
     {
       image: "4.png",
-      title: 'Bavarian burger',
-      text: 'Burger bun, beef patty, red onion, cheese, hunting sausage, barbecue sauce, cheese sauce, iceberg lettuce.',
+      title: $localize`Bavarian burger`,
+      text: $localize`Burger bun, beef patty, red onion, cheese, hunting sausage, barbecue sauce, cheese sauce, iceberg lettuce.`,
       price: 7,
       basePrice: 7,
       grams: 320
     },
     {
       image: "5.png",
-      title: 'Bacon cheeseburger',
-      text: 'Burger bun, beef cutlet, brisket, tomato, pickled cucumber, cheese, cheese sauce, ketchup, herbs.',
+      title: $localize`Bacon cheeseburger`,
+      text: $localize`Burger bun, beef cutlet, brisket, tomato, pickled cucumber, cheese, cheese sauce, ketchup, herbs.`,
       price: 8,
       basePrice: 8,
       grams: 360
     },
     {
       image: "6.png",
-      title: 'Indiana burger',
-      text: 'Burger bun, chicken cutlet, brisket, egg, pickled cucumber, crispy onion, ketchup, cheese sauce, mustard, greens.',
+      title: $localize`Indiana burger`,
+      text: $localize`Burger bun, chicken cutlet, brisket, egg, pickled cucumber, crispy onion, ketchup, cheese sauce, mustard, greens.`,
       price: 10,
       basePrice: 10,
       grams: 410
     },
     {
       image: "7.png",
-      title: 'Veggie burger',
-      text: 'Burger bun, veggie patty, red onion, cheese, fresh tomato, barbecue sauce, cheese sauce, iceberg lettuce.',
+      title: $localize`Veggie burger`,
+      text: $localize`Burger bun, veggie patty, red onion, cheese, fresh tomato, barbecue sauce, cheese sauce, iceberg lettuce.`,
       price: 8,
       basePrice: 8,
       grams: 290
     },
     {
       image: "8.png",
-      title: 'Weepy Joe',
-      text: 'Burger bun, beef patty, brisket, tomato, pickled cucumber, red onion, cheese, jalapeno pepper, ketchup, herbs.',
+      title: $localize`Weepy Joe`,
+      text: $localize`Burger bun, beef patty, brisket, tomato, pickled cucumber, red onion, cheese, jalapeno pepper, ketchup, herbs.`,
       price: 7,
       basePrice: 7,
       grams: 360
     },
     {
       image: "9.png",
-      title: 'Double cheeseburger',
-      text: 'Burger bun, two beef patties, double cheddar cheese, crispy onion, ketchup, cheese sauce, mustard, herbs.',
+      title: $localize`Double cheeseburger`,
+      text: $localize`Burger bun, two beef patties, double cheddar cheese, crispy onion, ketchup, cheese sauce, mustard, herbs.`,
       price: 11,
       basePrice: 11,
       grams: 420
     },
     {
       image: "10.png",
-      title: 'Fresh-burger',
-      text: 'Burger bun, beef patty, bacon, cheddar cheese, egg, salami, barbecue sauce, cheese sauce, iceberg lettuce, fresh tomato.',
+      title: $localize`Fresh-burger`,
+      text: $localize`Burger bun, beef patty, bacon, cheddar cheese, egg, salami, barbecue sauce, cheese sauce, iceberg lettuce, fresh tomato.`,
       price: 9,
       basePrice: 9,
       grams: 310
     },
     {
       image: "11.png",
-      title: 'Zucchini burger',
-      text: 'Burger bun, veggie chickpea patty, grilled zucchini, tomato, pickled cucumber, cheese, mustard sauce, ketchup, herbs.',
+      title: $localize`Zucchini burger`,
+      text: $localize`Burger bun, veggie chickpea patty, grilled zucchini, tomato, pickled cucumber, cheese, mustard sauce, ketchup, herbs.`,
       price: 8,
       basePrice: 8,
       grams: 330
     },
     {
       image: "12.png",
-      title: 'Double Cheddar Burger',
-      text: 'Burger bun, beef patty, brisket, red onion, pickled cucumber, tomato, ketchup, double cheddar cheese, mustard, herbs.',
+      title: $localize`Double Cheddar Burger`,
+      text: $localize`Burger bun, beef patty, brisket, red onion, pickled cucumber, tomato, ketchup, double cheddar cheese, mustard, herbs.`,
       price: 9,
       basePrice: 9,
       grams: 370
     },
     {
       image: "13.png",
-      title: 'Black White Light',
-      text: 'Black and white burger bun, beef patty, Parmesan cheese, hot sauce.',
+      title: $localize`Black White Light`,
+      text: $localize`Black and white burger bun, beef patty, Parmesan cheese, hot sauce.`,
       price: 12,
       basePrice: 12,
       grams: 290
     },
     {
       image: "14.png",
-      title: 'Black White Bacon',
-      text: 'Black and white burger bun, beef patty, bacon, Cheddar cheese, iceberg lettuce, sauce, cucumber.',
+      title: $localize`Black White Bacon`,
+      text: $localize`Black and white burger bun, beef patty, bacon, Cheddar cheese, iceberg lettuce, sauce, cucumber.`,
       price: 13,
       basePrice: 13,
       grams: 310
     },
     {
       image: "15.png",
-      title: 'Black White Max',
-      text: 'Black and white burger bun, beef patty, Cheddar cheese, iceberg lettuce, egg, black sauce.',
+      title: $localize`Black White Max`,
+      text: $localize`Black and white burger bun, beef patty, Cheddar cheese, iceberg lettuce, egg, black sauce.`,
       price: 16,
       basePrice: 16,
       grams: 330
     },
     {
       image: "16.png",
-      title: 'Black Star Light',
-      text: 'Black burger bun, chicken cutlet, egg, iceberg lettuce, fresh tomato, cucumber, Black sauce.',
+      title: $localize`Black Star Light`,
+      text: $localize`Black burger bun, chicken cutlet, egg, iceberg lettuce, fresh tomato, cucumber, Black sauce.`,
       price: 13,
       basePrice: 13,
       grams: 290
     },
     {
       image: "17.png",
-      title: 'Black Star',
-      text: 'Black burger bun, beef patty, Cheddar cheese, iceberg lettuce, fresh tomato, cucumber.',
+      title: $localize`Black Star`,
+      text: $localize`Black burger bun, beef patty, Cheddar cheese, iceberg lettuce, fresh tomato, cucumber.`,
       price: 14,
       basePrice: 14,
       grams: 310
     },
     {
       image: "18.png",
-      title: 'Black Star Max',
-      text: 'Black burger bun, beef cutlet, Bacon, Onion, Cheddar cheese, iceberg lettuce, fresh tomato, ketchup.',
+      title: $localize`Black Star Max`,
+      text: $localize`Black burger bun, beef cutlet, Bacon, Onion, Cheddar cheese, iceberg lettuce, fresh tomato, ketchup.`,
       price: 15,
       basePrice: 15,
       grams: 330
@@ -205,6 +214,23 @@ export class AppComponent {
           }
         )
     }
+  }
+
+  public changeLanguage () {
+    let newLanguage = 'English'
+    let code = 'en-US';
+
+      if (this.language === 'English') {
+        newLanguage = 'Русский'
+          code = 'ru'
+      } else if (this.language === 'Русский'){
+        newLanguage = 'English'
+          code = 'en-US'
+
+      }
+      this.language = newLanguage
+      console.log(this.language, code)
+      return code
   }
 
   public changeCurrency() {
